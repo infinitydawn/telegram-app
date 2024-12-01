7ccd937cfe7a3d3cbb56b4e32f9e923297e14765
Mikhail Nikulin <Michaelnikulinusa@gmail.com>
2024-12-01 00:43:16 -0500

    deploy test 3

--- src/Services/telegram.js
+++ src/Services/telegram.js
@@ -1,6 +1,8 @@
 export function useTelegram() {
     const tg = window.Telegram.WebApp
 
+    alert(tg)
+
     return {
         tg,
         user: tg.uinitDataUnsafe?.user, // only works inside Telegram, browser will show 4040
